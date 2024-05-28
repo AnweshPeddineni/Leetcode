a0f1c713af630147a3e21d97de26c8905b8ff749
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-05-28 15:40:34 -0400

    Time: 143 ms (36.81%), Space: 46.3 MB (13.99%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,33 +1,22 @@
 class Solution {
     public String longestPalindrome(String s) {
+        
         int n = s.length();
-        if (n == 0) return "";
-
         boolean[][] dp = new boolean[n][n];
-        int[] ans = {0, 0};
-
-        // Initialize single character substrings as palindromes
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-        }
-
-        // Iterate over all possible lengths of substrings
-        for (int diff = 1; diff < n; diff++) {
-            for (int i = 0; i < n - diff; i++) {
-                int j = i + diff;
-
-                if (s.charAt(i) == s.charAt(j)) {
-                    if (diff == 1 || dp[i + 1][j - 1]) {
-                        dp[i][j] = true;
-                        ans[0] = i;
-                        ans[1] = j;
-                    }
+        
+        int start = 0;
+        int end = 0;
+        
+        for(int i = n-1; i>=0; i--){
+            for(int j = i+1; j<n; j++){
+                dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1]);
+                if(dp[i][j]  && j - i > end - start){
+                    start = i;
+                    end = j;
                 }
             }
         }
-
-        int i = ans[0];
-        int j = ans[1];
-        return s.substring(i, j + 1);
+        
+        return s.substring(start, end+1);
     }
 }

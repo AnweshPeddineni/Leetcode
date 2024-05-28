4e600ddd63273238c329c65fdcdc2eae0f842f46
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-05-28 16:20:31 -0400

    Time: 145 ms (36.48%), Space: 46.5 MB (11.83%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -7,7 +7,7 @@ class Solution {
         int start = 0;
         int end = 0;
         
-        for(int i = n-1; i>=0; i--){
+        for(int i = n-1 ; i >= 0; i--){
             for(int j = i+1; j<n; j++){
                 dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1]);
                 if(dp[i][j]  && j - i > end - start){

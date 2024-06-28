691f4bcb8e1a6b45c7bf2e1b82dea3f9dae1eec3
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-06-27 23:21:27 -0400

    Time: 64 ms (55.13%), Space: 63.5 MB (6.05%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.java
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        
+        int n = temperatures.length;
+        int[] answer = new int[n];
         Stack<Integer> stack = new Stack<>();
         
-        int[] result = new int[temperatures.length];
-    
-        
-        for(int i=0; i<temperatures.length; i++){
-            
-            while(!stack.isEmpty() && temperatures[stack.peek()] < temperatures[i]){
-                int index = stack.pop();
-                result[index] = i-index;
+        for (int currDay = 0; currDay < n; currDay++) {
+            int currentTemp = temperatures[currDay];
+            // Pop until the current day's temperature is not
+            // warmer than the temperature at the top of the stack
+            while (!stack.isEmpty() && currentTemp > temperatures[stack.peek()]) {
+                int prevDay = stack.pop();
+                answer[prevDay] = currDay - prevDay;
             }
-            stack.push(i);
+            stack.push(currDay);
         }
         
-        return result;
+        return answer;
     }
 }

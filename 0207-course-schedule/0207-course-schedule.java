9b66b46f20fc598796497cc4453952209ad69c94
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-10-15 23:37:55 -0700

    Time: 3 ms (98.00%), Space: 44.9 MB (69.17%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,51 +1,47 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        // Create an adjacency list to represent the graph
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < numCourses; i++) {
-            adj.add(new ArrayList<>());
+        
+        List<List<Integer>> adjList = new ArrayList<>();
+        
+        for(int i=0; i<numCourses; i++){
+            adjList.add(new ArrayList<>());
         }
-
-        // Fill the adjacency list with prerequisites
-        for (int i = 0; i < prerequisites.length; i++) {
-            adj.get(prerequisites[i][0]).add(prerequisites[i][1]);
+        
+        for(int i=0; i<prerequisites.length; i++){
+            adjList.get(prerequisites[i][0]).add(prerequisites[i][1]);
         }
-
-        // Create an array to track the visit state of each course
-        // 0 - not visited, 1 - visited, 2 - currently visiting
+        
+        // 0->not visited, 1->currently visiting, 2->visited
         int[] visited = new int[numCourses];
-        for (int i = 0; i < numCourses; i++) {
-            // If not visited, check for cycles
-            if (visited[i] == 0) {
-                if (isCyclic(adj, visited, i)) {
-                    // If a cycle is found, return false
-                    return false;
-                }
-            }
-        }
-        // If no cycles found, return true
-        return true;
+        
+         for(int i=0; i<numCourses; i++){
+             if(visited[i] == 0){
+                 if(isCyclic(adjList, visited, i)){
+                     return false;
+                 }
+             }
+         } 
+         return true;
+        
     }
-
-    private boolean isCyclic(List<List<Integer>> adj, int[] visited, int curr) {
-        // If currently visiting, it's a cycle
-        if (visited[curr] == 2) {
+    
+    public boolean isCyclic(List<List<Integer>> adjList, int[] visited, int curr){
+        if(visited[curr] == 1){
             return true;
         }
-
-        // Mark the current course as currently visiting
-        visited[curr] = 2;
-        for (int i = 0; i < adj.get(curr).size(); i++) {
-            // If the neighbor is not fully visited
-            if (visited[adj.get(curr).get(i)] != 1) {
-                // Recursively visit the neighbor
-                if (isCyclic(adj, visited, adj.get(curr).get(i))) {
+        
+        visited[curr] = 1;
+        
+        for(int num : adjList.get(curr)){
+            if(visited[num] != 2){
+                if(isCyclic(adjList, visited, num)){
                     return true;
                 }
-            }
+            } 
         }
-        // After visiting all neighbors, mark as fully visited
-        visited[curr] = 1;
+        
+        visited[curr] = 2;
         return false;
     }
+    
 }

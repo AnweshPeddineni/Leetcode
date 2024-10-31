8a6dad2826a952b3804cf6adad81c65eb4923050
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-10-31 15:07:42 -0700

    Time: 47 ms (75.25%), Space: 73.6 MB (46.21%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,7 +1,7 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        Arrays.sort(intervals, (a,b) -> (a[0]-b[0]));
         
         int remove = 0;
         int start = intervals[0][0];
@@ -10,7 +10,8 @@ class Solution {
         for(int i=1; i<intervals.length; i++){
             if(intervals[i][0] < end){
                 remove++;
-                // focus on the below line, the below is the main logic responsible for what to keep and what to remove. The if condition is just checking if the overlap exists.
+                
+                // when there is an overlap always remove the interval that is larger among the two.
                 end = Math.min(end, intervals[i][1]);
             }else{
                 start = intervals[i][0];

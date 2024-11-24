de57b1fd266ae021347efbe53dbfe27c4abf4148
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2024-11-23 20:54:37 -0800

    Time: 7 ms (65.96%), Space: 44.4 MB (81.15%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,15 +7,26 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
+ // Use post order traversal left, right, root
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        
-        if (root == null) return null;
-        if (root.val == p.val) return p;
-        if (root.val == q.val) return q;
+
+        if(root == null) return null;
+
+        if(root.val == p.val) return p;
+        if(root.val == q.val) return q;
+
         TreeNode left = lowestCommonAncestor(root.left, p, q);
         TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) return root;
-        return (left == null) ? right : left;
+
+        if(left != null && right != null) return root;
+
+        if(left == null){
+            return right;
+        }else{
+            return left;
+        }
+
     }
 }

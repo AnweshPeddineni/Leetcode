df4e91a00506507529b32f4027ef8093e0ee0368
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-02-05 18:08:10 -0800

    Time: 3 ms (50.11%), Space: 42.1 MB (34.01%) - LeetHub

--- 0682-baseball-game/0682-baseball-game.java
+++ 0682-baseball-game/0682-baseball-game.java
@@ -1,41 +1,32 @@
 class Solution {
     public int calPoints(String[] operations) {
-        Stack<String> stack = new Stack<>();
-
-        int scoresSum = 0;
-
-        for(int i=0; i<operations.length; i++){
-
-            if(operations[i].equals("+")){
-               String prevOne = stack.pop();
-               String prevTwo = stack.pop();
-               int result = Integer.parseInt(prevOne)+Integer.parseInt(prevTwo);
-               String resultStr = Integer.toString(result);
-               stack.push(prevTwo);
-               stack.push(prevOne);
-               stack.push(resultStr);
-
-            }else if(operations[i].equals("D")){
-               String prevOne = stack.peek();
-               int result = Integer.parseInt(prevOne)*2;
-               String resultStr = Integer.toString(result);
-               stack.push(resultStr);
-
-            }else if(operations[i].equals("C")){
-               stack.pop();
-            }else{
-               stack.add(operations[i]);
-            }
-            
-        }
-
+        int n = operations.length;
+        Stack<Integer> st = new Stack<>();
         
-
-        while(!stack.isEmpty()){
-            scoresSum += Integer.parseInt(stack.pop());
+        for(int i = 0; i < n; i++) {
+            if(operations[i].equals("+")) {
+                int a = st.pop();
+                int b = st.peek();
+                st.push(a);
+                st.push(a + b);
+            }
+            else if(operations[i].equals("C")) {
+                st.pop();  
+            }
+            else if(operations[i].equals("D")) {
+                int a = st.peek();
+                st.push(a * 2);
+            }
+            else {
+                int a = Integer.parseInt(operations[i]);
+                st.push(a);
+            }
         }
-
-        return scoresSum;
-
+        
+        int sum = 0;
+        while(!st.isEmpty()) {   
+            sum += st.pop();
+        }
+        return sum;
     }
 }

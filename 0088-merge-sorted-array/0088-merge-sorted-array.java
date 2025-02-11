7a568bb0f8dbc6013e556b22087832eea5bbb1e6
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-02-10 22:22:37 -0800

    Time: 110 ms (2.76%), Space: 42.3 MB (59.42%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,26 +1,36 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1;       // Pointer for nums1
-        int p2 = n - 1;       // Pointer for nums2
-        int p = m + n - 1;    // Pointer for placement in nums1
+        
+        int i=0;
+        int j=0;
 
-        // Merge nums1 and nums2 starting from the end
-        while (p1 >= 0 && p2 >= 0) {
-            if (nums1[p1] > nums2[p2]) {
-                nums1[p] = nums1[p1]; // Place larger element
-                p1--;
-            } else {
-                nums1[p] = nums2[p2];
-                p2--;
+        while(i<m && j<n){
+            if(nums1[i] > nums2[j]){
+                shiftOne(nums1, i, m);
+                nums1[i] = nums2[j];
+                j++;
+                m++;
             }
-            p--; // Move placement pointer backward
+            
+            i++;
+        
         }
+        
+        
+        while(j<n){
+            nums1[i] = nums2[j];
+            i++;
+            j++;
+        }
+  
 
-        // If there are remaining elements in nums2, copy them
-        while (p2 >= 0) {
-            nums1[p] = nums2[p2];
-            p2--;
-            p--;
-        }
+    }
+
+    public void shiftOne(int[] nums, int start, int m){
+      
+       for(int i=m-1; i>=start; i--){
+          nums[i+1] = nums[i];
+       }
+      
     }
 }

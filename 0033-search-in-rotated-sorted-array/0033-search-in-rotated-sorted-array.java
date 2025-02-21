2b737b3e0e528cbc6edb8bbd6038856bacdf9ac5
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-02-21 01:35:11 -0800

    Time: 0 ms (100%), Space: 41.3 MB (99.86%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,48 +1,30 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) return -1;
+        int l = 0;
+        int r = nums.length - 1;
         
-        int left = 0;
-        int right = nums.length - 1;
-        
-        // Step 1: Find the pivot (smallest element index)
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > nums[right]) {
-                // Pivot is in the right half
-                left = mid + 1;
-            } else {
-                // Pivot is in the left half (including mid)
-                right = mid;
-            }
-        }
-        
-        int pivot = left;
-        left = 0;
-        right = nums.length - 1;
-        
-        // Step 2: Determine the search boundaries
-        if (target >= nums[pivot] && target <= nums[right]) {
-            left = pivot;
-        } else {
-            right = pivot - 1;
-        }
-        
-        // Step 3: Perform standard binary search
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
-        }
-        
-        // Target not found
-        return -1;
+       while(l<=r){
+           
+          int mid = (l+r)/2;
+           
+           if(nums[mid] == target){
+               return mid;
+           }
+           //left sorted
+           if(nums[l] <= nums[mid]){
+               if(target >= nums[l] && target < nums[mid]){
+                   r = mid-1;
+               }else{
+                   l = mid+1;
+               }
+           }else{//right sorted
+               if(target > nums[mid] && target <= nums[r]){
+                   l = mid+1;
+               }else{
+                   r = mid-1;
+               }
+           }
+       }    
+       return -1; // Target was not found
     }
 }

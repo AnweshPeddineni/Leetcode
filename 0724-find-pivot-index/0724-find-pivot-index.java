5aa29fb3992c955580d049da22ed00ec3743dbf2
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-03-01 19:31:44 -0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -23,12 +23,15 @@ class Solution {
         
     }
 }
-
-//                      i               
+// tS = 28
+//                  totalSum - leftSum - nums[i]
+//                  i-------            
 // Input: nums = [1,7,3,6,5,6]
+                 ...
+//               leftSum
+
 //   prefixSum = [0,1,8,11]
 
 //  
 // return p
 
-// totalSum = 28

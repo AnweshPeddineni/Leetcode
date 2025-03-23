a42fb0b1d96271a813ec84a4a5929f474d9f6cb5
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-03-23 11:44:00 -0700

    Time: 1973 ms (6.41%), Space: 45.3 MB (42.23%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -1,22 +1,46 @@
 class Solution {
     public String longestPalindrome(String s) {
         
-        int n = s.length();
-        boolean[][] dp = new boolean[n][n];
-        
-        int start = 0;
-        int end = 0;
-        
-        for(int i = n-1 ; i >= 0; i--){
-            for(int j = i+1; j<n; j++){
-                dp[i][j] = s.charAt(i) == s.charAt(j) && (j-i <= 2 || dp[i+1][j-1]);
-                if(dp[i][j]  && j - i > end - start){
-                    start = i;
-                    end = j;
+        int i = 0;
+        int j = 0;
+
+        String result = "";
+
+        while (i < s.length() && j < s.length()) {
+
+            // Check if s[i...j] is a palindrome
+            if (checkPalindrome(s, i, j)) {
+                String currResult = s.substring(i, j + 1);
+
+                // Update result if we found a longer palindrome
+                if (currResult.length() > result.length()) {
+                    result = currResult;
                 }
             }
+
+            // Move the `j` pointer to explore longer substrings
+            j++;
+
+            // If `j` reaches the end and no valid palindrome is found,
+            // reset `i` to the next index and start over.
+            if (j == s.length()) {
+                i++;
+                j = i; // Start j again from i
+            }
         }
-        
-        return s.substring(start, end+1);
+
+        return result;
+    }
+
+    // Palindrome checking function
+    public boolean checkPalindrome(String s, int i, int j) {
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) {
+                return false; // Found mismatch, not a palindrome
+            }
+            i++;
+            j--;
+        }
+        return true; // All characters matched, it's a palindrome
     }
 }

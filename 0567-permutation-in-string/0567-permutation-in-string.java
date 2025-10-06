30314a268306b7879469415c4ee7568853271a1b
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-10-06 13:49:27 -0700

    Time: 7 ms (68.79%), Space: 42.7 MB (81.21%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,39 +1,44 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        
-        HashMap<Character, Integer> s1Map = new HashMap<>();
 
-        HashMap<Character, Integer> s2WindowMap = new HashMap<>();
-
-
-        for(int i=0; i<s1.length(); i++){
-            s1Map.put(s1.charAt(i), s1Map.getOrDefault(s1.charAt(i),0)+1);
+        // If s1 is longer than s2, s2 cannot contain a permutation of s1
+        if (s1.length() > s2.length()) {
+            return false;
         }
 
+        // s1Map stores the frequency count of characters in s1
+        int[] s1Map = new int[26];
+        for (char ch : s1.toCharArray()) {
+            s1Map[ch - 'a']++;
+        }
 
-        int left = 0;
-        int right = 0;
-
-        while(right < s2.length()){
-          
-            s2WindowMap.put(s2.charAt(right), s2WindowMap.getOrDefault(s2.charAt(right),0)+1);
+        // windowMap stores the frequency count of characters in the current sliding window of s2
+        int[] windowMap = new int[26];
         
-            while(right - left + 1 > s1.length()){
-                char leftChar = s2.charAt(left);
-                s2WindowMap.put(leftChar, s2WindowMap.get(leftChar) - 1);
-                if (s2WindowMap.get(leftChar) == 0) {
-                    s2WindowMap.remove(leftChar);  // Remove entry if count reaches 0
+        int l = 0; // Left pointer of the sliding window
+        
+        // Iterate with the right pointer 'r' through s2
+        for (int r = 0; r < s2.length(); r++) {
+            // Expand the window: Add the current character s2.charAt(r) to windowMap
+            windowMap[s2.charAt(r) - 'a']++;
+
+            // Shrink the window from the left if its size exceeds s1.length()
+            // This ensures our sliding window always maintains a size of at most s1.length()
+            while (r - l + 1 > s1.length()) {
+                windowMap[s2.charAt(l) - 'a']--; // Remove the character at the left pointer
+                l++; // Move the left pointer to the right
+            }
+
+            // Check for a permutation only when the window size matches s1.length()
+            if (r - l + 1 == s1.length()) {
+                // If the frequency maps match, we found a permutation
+                if (Arrays.equals(s1Map, windowMap)) {
+                    return true;
                 }
-                left++;
             }
-
-            if(s1Map.equals(s2WindowMap)){
-                return true;
-            }
-
-            right++;
         }
 
+        // If no permutation is found after checking all possible windows
         return false;
     }
 }

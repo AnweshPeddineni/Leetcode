0855a7ce4a9c22722a18a86ad7611588f86a34a9
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-10-25 03:21:20 -0700

    Time: 0 ms (100%), Space: 42.1 MB (54.46%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,39 +1,41 @@
 class Solution {
     public int search(int[] nums, int target) {
+        //step1 - finding pivot
+        int left = 0;
+        int right = nums.length - 1;
+        int mid = 0;
+        while(left < right){
+            mid = (left + right)/2;
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
         
-       int left = 0;
-       int right = nums.length-1;
-
-       int mid = 0;
-
-       while(left <= right){
-
-         mid = left + (right-left)/2;
-
-         if(nums[mid] == target){
-            return mid;
-         }
-
-         if(nums[left] <= nums[mid]){
-            // left sorted
-            if(target >= nums[left] && target < nums[mid]){
-               right = mid-1;
-            }else{
-               left = mid+1;
+        //step-2 finding in which of the sorted array should I search
+        int start = left;
+        left = 0;
+        right = nums.length - 1;
+        
+        if(nums[start] <= target && nums[right] >= target){
+            left = start;
+        } else{
+            right = start;
+        }
+        
+        //Step-3
+        while(left <= right){
+            mid = (left + right)/2;
+            if(nums[mid] > target){
+                right = mid - 1;
+            } else if(nums[mid] < target){
+                left = mid + 1;
+            } else {
+                return mid;
             }
-
-         }else{
-            // right sorted
-            if(target > nums[mid] && target <= nums[right]){
-              left = mid+1;
-            }else{
-              right = mid-1;
-            }
-
-         }
-
-       }
-
-       return -1;    
+        }
+        
+        return -1;
     }
 }

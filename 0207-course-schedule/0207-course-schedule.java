75a9a644ed4cac47779ef40ced2ec265c709eccd
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-10-31 21:58:27 -0700

    Time: 7 ms (44.46%), Space: 47.1 MB (6.33%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,39 +1,42 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> graph = new ArrayList<>();
-        int[] inDegree = new int[numCourses];
+        int[] indegree = new int[numCourses];
 
-        // Step 1: Build the graph and in-degree array
-        for (int i = 0; i < numCourses; i++) {
-            graph.add(new ArrayList<>());
-        }
-        for (int[] prereq : prerequisites) {
-            graph.get(prereq[1]).add(prereq[0]);
-            inDegree[prereq[0]]++;
+        List<List<Integer>> adjList = new ArrayList<>();
+
+        for(int i=0; i<numCourses; i++){
+            adjList.add(new ArrayList<>());
         }
 
-        // Step 2: Add nodes with in-degree 0 to the queue
+        for(int i=0; i<prerequisites.length; i++){
+            adjList.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++;
+        }
+
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegree[i] == 0) {
+        
+        for(int i=0; i<numCourses; i++){
+            if(indegree[i] == 0){
                 queue.offer(i);
             }
         }
 
-        // Step 3: Process the graph
-        int visitedCourses = 0;
-        while (!queue.isEmpty()) {
-            int course = queue.poll();
-            visitedCourses++;
-            for (int neighbor : graph.get(course)) {
-                inDegree[neighbor]--;
-                if (inDegree[neighbor] == 0) {
-                    queue.offer(neighbor);
-                }
-            }
-        }
+        int visited = 0;
 
-        // Step 4: Check if all courses were visited
-        return visitedCourses == numCourses;
+        while(!queue.isEmpty()){
+            for(int i=0; i<queue.size(); i++){
+                int currCourse = queue.poll();
+                visited++;
+                for(int neighbor : adjList.get(currCourse)){
+                    if(indegree[neighbor] > 0){
+                        indegree[neighbor]--;
+                        if(indegree[neighbor] == 0){
+                            queue.add(neighbor);
+                        }
+                    }
+                }
+            } 
+        }
+        return visited == numCourses;
     }
 }

ed8c523e0bc018b3396ff58843b08711dc9dbd1d
AnweshPeddineni <97170924+AnweshPeddineni@users.noreply.github.com>
2025-11-11 16:31:07 -0800

    Time: 1 ms (94.34%), Space: 45.7 MB (20.76%) - LeetHub

--- 0419-battleships-in-a-board/0419-battleships-in-a-board.java
+++ 0419-battleships-in-a-board/0419-battleships-in-a-board.java
@@ -1,23 +1,17 @@
 class Solution {
     public int countBattleships(char[][] board) {
-        int rows = board.length;
-        int cols = board[0].length;
+        int m = board.length, n = board[0].length;
         int count = 0;
-
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                // Check if this cell is part of a battleship
-                if (board[i][j] == 'X') {
-                    // Check if it's the start of a new battleship
-                    if (i > 0 && board[i - 1][j] == 'X') continue; // Skip if there's an 'X' above
-                    if (j > 0 && board[i][j - 1] == 'X') continue; // Skip if there's an 'X' to the left
-
-                    // This is a new battleship
-                    count++;
-                }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] != 'X') continue;
+                // if there's an 'X' above or to the left, this cell is a continuation of a ship
+                if (i > 0 && board[i - 1][j] == 'X') continue;
+                if (j > 0 && board[i][j - 1] == 'X') continue;
+                // otherwise it's the top-left of a ship
+                count++;
             }
         }
-
         return count;
     }
 }
